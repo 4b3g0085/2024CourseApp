8802866b63f86585871c2025d17cabc0290ccb35
4b3g0085 <4b3g0085@office.stust.edu.tw>
2024-12-03 10:22:51 +0800

    123

--- 2024CourseApp/2024CourseApp.cpp
+++ 2024CourseApp/2024CourseApp.cpp
@@ -1,11 +1,13 @@
-﻿
+﻿#include <string>
 #include <iostream>
 #include "Person.h"
+#include "Student.h"
+#include "Utility.h"
 using namespace std;
 
 int main()
 {
-	Person person1("A123456789", "Chen", "Amy", "F", "2000-01-01");
+	Person person1("A123456789", "Chen", "Jason", "M", "1999-01-01");
 
 	Person* person2 = new Person();
 	person2->setId("B987654321");
@@ -17,4 +19,7 @@ int main()
 	person1.display();
 	cout << endl;
 	person2->display();
+
+	Student student1("A123456789", "Chen", "Jason", "M", "1999-01-01", "S001", Department::ComputerSciece, ClassName::_1A);
+	student1.display();
 }
